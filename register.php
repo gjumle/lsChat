05bae3ba623e2b4b48260084e9f0d2c71ed714dc
Leoš Gjumija <gjumle@protonmail.com>
2023-02-23 09:10:17 +0100

    navbar

--- register.php
+++ register.php
@@ -1,19 +1,38 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Register</title>
+    <title>Contacts</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<nav>
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="contacts.php">Contacts</a></li>
-    <li><a href="schedule.php">Schedule</a></li>
-    <li><a href="statistics.php">Statistics</a></li>
-    <li><a href="logout.php">Logout</a></li>
-  </ul>
+<nav class="navbar">
+  <div class="navbar-container">
+    <a href="#" class="logo">lastSeen</a>
+    <ul class="navbar-menu">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="contacts.php">Contacts</a></li>
+      <li><a href="schedule.php">Schedule</a></li>
+      <li><a href="stats.php">Statistics</a></li>
+    </ul>
+    <div class="navbar-right">
+      <a href="logout.php">Logout</a>
+    </div>
+    <button class="navbar-toggle">
+      <span class="navbar-toggle-icon"></span>
+    </button>
+  </div>
+  <div class="navbar-mobile-menu">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="contacs.php">Contacts</a></li>
+      <li><a href="schedule.php">Schedule</a></li>
+      <li><a href="stats.php">Statistics</a></li>
+      <li><a href="logout.php">Logout</a></li>
+    </ul>
+  </div>
 </nav>
-
+</body>
+</html>
     <form action="register.php" method="POST">
         <input type="text" name="username" placeholder="Username">
         <input type="password" name="password" placeholder="Password">
---
+++ stats.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Contacts</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar">
+  <div class="navbar-container">
+    <a href="#" class="logo">lastSeen</a>
+    <ul class="navbar-menu">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="contacts.php">Contacts</a></li>
+      <li><a href="schedule.php">Schedule</a></li>
+      <li><a href="stats.php">Statistics</a></li>
+    </ul>
+    <div class="navbar-right">
+      <a href="logout.php">Logout</a>
+    </div>
+    <button class="navbar-toggle">
+      <span class="navbar-toggle-icon"></span>
+    </button>
+  </div>
+  <div class="navbar-mobile-menu">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="contacs.php">Contacts</a></li>
+      <li><a href="schedule.php">Schedule</a></li>
+      <li><a href="stats.php">Statistics</a></li>
+      <li><a href="logout.php">Logout</a></li>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
